548243429f928fe386846ff72505a18a49ed1806
ayan878 <ayanraza197@gmail.com>
2024-02-21 00:56:42 +0530

    Episode-01-Inception

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+
+// React create a element (h1) is an Object 
 const heading = React.createElement(
   "h1",
-  { id: "heading"},
+  { id: "heading" },
   "Hello React by external Js File"
 );
+console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
