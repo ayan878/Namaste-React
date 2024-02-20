50926ba507c879eb9e56da05efa17b613e52f1c8
ayan878 <ayanraza197@gmail.com>
2024-02-21 01:06:46 +0530

    Episode-01-Inception

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-
-// React create a element (h1) is an Object 
+// Create a React element (h1) as an object with specific attributes
 const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello React by external Js File"
+  "h1", // Element type
+  { id: "heading" }, // Element attributes
+  "Hello React by external Js File" // Element content
 );
-console.log(heading);
+
+console.log(heading); // Output the created React element to the console
+
+// Create a root for React rendering, select the DOM element with id "root"
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the 'heading' React element into the root
+// The render method converts the 'heading' object into an h1 tag and places it in the DOM
 root.render(heading);
