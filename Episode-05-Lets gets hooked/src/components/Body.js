5d5c05cfcee7e2ecf6d744190d5085aff40c647c
ayan878 <ayanraza197@gmail.com>
2024-02-23 02:27:55 +0530

    Episode-05-Lets gets hooked

--- Episode-05-Lets gets hooked/src/components/Body.js
+++ Episode-05-Lets gets hooked/src/components/Body.js
@@ -1,13 +1,29 @@
+import React, { useState } from "react";
 import { resList } from "../utils/data.js";
 import { RestaurentCard } from "./Restaurent.js";
+
 export const Body = () => {
+  
+  const [filteredResList, setFilteredResList] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">
-        <input type="search" className="search" placeholder="Search..." />
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            // Filter the resList based on the condition
+            const filteredList = resList.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setFilteredResList(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
       </div>
       <div className="res-container">
-        {resList.map((res) => (
+        {filteredResList.map((res) => (
           <RestaurentCard key={res.id} res={res} />
         ))}
       </div>
--- Episode-05-Lets gets hooked/src/components/Restaurent.js
+++ Episode-05-Lets gets hooked/src/components/Restaurent.js
@@ -3,17 +3,11 @@ import { CDN_URL } from "../utils/constants";
 export const RestaurentCard = ({ res }) => {
   return (
     <div className="res-card">
-      <img
-        className="res-logo"
-        src={
-          CDN_URL +
-          res.data.cloudinaryImageId
-        }
-      />
+      <img className="res-logo" src={CDN_URL + res.data.cloudinaryImageId} />
       <h3>{res.data.name}</h3>
       <h4>{res.data.cuisines.join(",")}</h4>
       <h4>₹{res.data.costForTwo / 100}</h4>
-      <h4>{res.data.rating}</h4>
+      <h4>{res.data.avgRating}</h4>
       <h4>{res.data.deliveryTime}min</h4>
     </div>
   );
