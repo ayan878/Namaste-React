2cd6f68013ad85567a22ca428c73aa25f720d42d
ayan878 <ayanraza197@gmail.com>
2024-02-22 23:14:52 +0530

    Episode-04-Talk is cheap,show me the code

--- Episode-04-talk is cheap, show me the code/App.js
+++ Episode-04-talk is cheap, show me the code/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { resList } from "./data.js";
 const Header = () => {
   return (
     <div className="header">
@@ -22,10 +22,21 @@ const Header = () => {
   );
 };
 
-const RestaurentCard = () => {
+const RestaurentCard = ({res}) => {
   return (
     <div className="res-card">
-      <h3>Meghna Food</h3>
+      <img
+        className="res-logo"
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/" +
+          res.data.cloudinaryImageId
+        }
+      />
+      <h3>{res.data.name}</h3>
+      <h4>{res.data.cuisines.join(",")}</h4>
+      <h4>Rs{res.data.costForTwo/100}</h4>
+      <h4>{res.data.rating}</h4>
+      <h4>{res.data.deliveryTime}min</h4>
     </div>
   );
 };
@@ -33,21 +44,26 @@ const Body = () => {
   return (
     <div className="body">
       <div className="search">
-        <input type="search" className="search" placeholder="Search..."/>
+        <input type="search" className="search" placeholder="Search..." />
       </div>
-      <div className="res-container"><RestaurentCard/></div>
+      <div className="res-container">
+        {resList.map((res) => (
+          <RestaurentCard key={res.id} res={res} />
+        ))}
+        {/* <RestaurentCard res={resList}/> */}
+      </div>
     </div>
   );
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const Applayout = () => {
+const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body/>
+      <Body />
     </div>
   );
 };
 
-root.render(<Applayout />);
+root.render(<AppLayout />);
